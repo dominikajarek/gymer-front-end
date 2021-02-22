7344e425203dab229ad9f748f60aeb54100a85a7
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-02-22 17:45:10 +0100

    Add comments to the test code

--- src/components/main/Login.js
+++ src/components/main/Login.js
@@ -13,13 +13,21 @@ function Login() {
 
         axios.post("/api/login", loginDetails)
             .then(response => {
+                // show message fron JsonResponse object
                 console.log(response.data);
                 alert(response.data.message);
+
+                // set up JSON Web Token to the variable
+                const jwt = response.headers.authorization;
+                console.log(jwt);
+
+                // if error field === false should redirect to the homepage
                 if (response.data.error === false) {
                     // redirect to homepage with logged in status
                 }
             })
             .catch(reason => {
+                // if there is error with credentials like 400 bad request or else with message in json format
                 if (reason.response) {
                     console.log(reason.response.data);
                     alert(reason.response.data.message);
