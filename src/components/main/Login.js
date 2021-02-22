6cacaab3289964d5cbf8bc643433be6c81bfe503
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-02-22 16:53:47 +0100

    Change proxy address and Add simple login form request handler

--- src/components/main/Login.js
+++ src/components/main/Login.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
 import '../../styles/login.css';
+import axios from "axios";
 
 function Login() {
-    return (
-        <div className='login-container'>
 
+    function handleSubmit() {
+        const loginDetails = {
+            "email": document.getElementById('email').value,
+            "password": document.getElementById('password').value
+        }
+
+        axios.post("/api/login", loginDetails)
+            .then(response => {
+                console.log(response.data);
+                alert(response.data.message);
+                if (response.data.error === false) {
+                    // redirect to homepage with logged in status
+                }
+            })
+            .catch(reason => {
+                if (reason.response) {
+                    console.log(reason.response.data);
+                    alert(reason.response.data.message);
+                }
+            });
+    }
+
+    return (
+        <div>
+            <label htmlFor="email">Email:</label><br/>
+            <input type="text" id="email" name="email" defaultValue={"email"}/><br/>
+            <label htmlFor="password">Password:</label><br/>
+            <input type="password" id="password" name="password" defaultValue={"password"}/><br/>
+            <input onClick={handleSubmit} defaultValue={"Submit"}/>
         </div>
     );
 }
