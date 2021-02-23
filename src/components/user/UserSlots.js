353eab022dbd97ab86fe9c5fe2e5f6942e63a701
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-02-23 16:57:20 +0100

    Add showing up and manipulate with resign button on user-slots site

--- src/components/user/UserSlots.js
+++ src/components/user/UserSlots.js
@@ -1,11 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import axios from "axios";
 
+import '../../index.css';
 import '../../styles/userSlots.css';
+import {useHistory} from "react-router-dom";
 
 export const UserSlots = () => {
 
+    const [user, setUser] = useState([]);
     const [slots, setSlots] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const history = useHistory();
+    const handleRemoveSlot = useCallback(() => window.location.reload(), [history]);
 
     let config = {
         headers: {
@@ -13,13 +20,18 @@ export const UserSlots = () => {
         }
     }
 
+    useEffect(() => {
+        getActiveUser();
+    }, []);
+
     const getActiveUser = () => {
         axios.get('/api/me', config)
             .then(response => {
+                setUser(response.data);
                 getActiveUsersSlots(response.data.id);
             }).catch(reason => {
-                console.log(reason.response);
-            });
+            console.log(reason.response);
+        });
     }
 
     const getActiveUsersSlots = (id) => {
@@ -27,39 +39,57 @@ export const UserSlots = () => {
         axios.get(getSlotsUrl, config)
             .then(response => {
                 if (response.data != null) {
-                    const resultSlots = [];
-                    response.data.get("_embedded").get("slotDTOList").forEach(slot => {
-                        resultSlots.push(slot);
-                    });
-                    setSlots(resultSlots);
+                    setSlots(response.data._embedded.slotDTOList);
                 }
 
             }).catch(reason => {
-                if (reason.response != null) {
-                    console.log(reason.response);
-                }
-            });
+            if (reason.response != null) {
+                console.log(reason.response);
+            }
+        });
     }
 
-    useEffect(() => {
-        getActiveUser();
-    }, []);
-
     const listOfSlots = slots.map(slot =>
-        <div key={slot.id}>
-            <h3>{slot.slotType}</h3>
-            <p>{slot.description}</p>
-            <span>Time: {slot.startTime} - {slot.endTime}</span>
-            <span>Date: {slot.date}</span>
-            <span>{slot.private}</span>
-            <span>{slot.size}</span>
-            <button>RESIGN</button>
+        <div className="table" key={slot.id}>
+            <div className="row">
+                <div className="value">{slot.slotType}</div>
+                <div className="value">Time: {slot.startTime} - {slot.endTime}</div>
+                <div className="value">Date: {slot.date}</div>
+            </div>
+            <div className="row">
+                <div className="value">{slot.description}</div>
+                <div className="value">Is private: {slot.private}</div>
+                <div className="value">Size: {slot.size}</div>
+            </div>
+            <button className="row" onClick={() => resignPopup(slot.id)}>RESIGN</button>
         </div>
     );
 
-    return (
-        <div>
-            <div>{listOfSlots}</div>
-        </div>
-    );
+    const resignPopup = (id) => {
+        if (window.confirm('Really go to another page?')) {
+            resignRequest(id);
+        } else {
+
+        }
+    }
+
+    const resignRequest = (slotId) => {
+        const body = {
+            "userId": user.id,
+            "slotId": slotId,
+            "cancel": true
+        }
+        const removeSlotsUrl = '/api/slotuser/' + slotId + '/reservation/user';
+        axios.post(removeSlotsUrl, body, config)
+            .then(response => {
+                if (response.data.error === false) {
+                    setMessage(response.data.message);
+                    handleRemoveSlot();
+                }
+            }).catch(reason => {
+            console.log(reason.response);
+        });
+    }
+
+    return (<div className="content"><h2>{message}</h2>{listOfSlots}</div>);
 }
