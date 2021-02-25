758f96bda8caef03f9bc348d2156144b6bf6c6d5
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-02-25 23:58:23 +0100

    Add activation feature

---
+++ src/components/main/Activation.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import {useLocation} from 'react-router-dom';
+import queryString from 'query-string';
+
+import axios from "axios";
+
+export function Activation() {
+    const [message, setMessage] = useState('');
+    const {search} = useLocation();
+    const {code} = queryString.parse(search)
+
+    useEffect(() => {
+        axios.get(`/api/verify?code=${code}`)
+            .then(response => {
+                setMessage(response.data.message);
+            })
+    }, []);
+
+
+    return (
+        <div>
+            <h3>{message}</h3>
+        </div>
+    );
+}
