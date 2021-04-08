55cd3fa716c2fc95174f14ccb5707589b6e02ce5
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-04-09 00:22:07 +0200

    Add partner slots list with delete feature. Add new slot not working yet

---
+++ src/components/partner/settings/AddNewSlot.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const AddNewSlot = (props) => {
+    return (
+        <div>
+            <h1>partner id = {props.partnerId}</h1>
+            <h2>feature still in progress</h2>
+        </div>
+    )
+}
